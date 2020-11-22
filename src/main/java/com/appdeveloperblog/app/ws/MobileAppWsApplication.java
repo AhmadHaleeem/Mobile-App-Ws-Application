8d13a1ef00d795e1c99c16c29979f8de372ebc77
AhmadHaleeem <coldman2099@gmail.com>
2020-11-22 15:30:37 +0100

    adding_context_path_to_the_application

--- src/main/java/com/appdeveloperblog/app/ws/MobileAppWsApplication.java
+++ src/main/java/com/appdeveloperblog/app/ws/MobileAppWsApplication.java
@@ -2,9 +2,16 @@ package com.appdeveloperblog.app.ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MobileAppWsApplication {
+public class MobileAppWsApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MobileAppWsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MobileAppWsApplication.class, args);
